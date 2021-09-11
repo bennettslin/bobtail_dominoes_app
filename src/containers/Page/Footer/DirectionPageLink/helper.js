0b28759738878885c20a8125510571519bb4c75e
Bennett Lin <bennettslin@gmail.com>
2021-09-11 15:43:08 -0700

    Fix tabbed page footer.

--- src/containers/Page/Footer/DirectionPageLink/helper.js
+++ src/containers/Page/Footer/DirectionPageLink/helper.js
@@ -5,15 +5,21 @@ export const getDirectionPage = ({
     pages,
     selectedPage,
 }) => {
+    if (!pages) {
+        return null
+    }
+
     const
         childPage = getChildPageFromPath(selectedPage),
-        pageIndex = pages.findIndex(({ id, date: { month, day } = {} }) => (
+        foundIndex = pages.findIndex(({ id, date: { month, day } = {} }) => (
             childPage === (
                 month && day ?
                     `${month}-${day}-${id}` :
                     id
             )
-        ))
+        )),
+        // If no index found, default to first page.
+        pageIndex = foundIndex === -1 ? 0 : foundIndex
 
     if (direction === -1 && pageIndex > 0) {
         return pages[pageIndex - 1]
