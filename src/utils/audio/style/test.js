719431926848b5d92462d9a37aabb2c573ac6572
Bennett Lin <bennettslin@gmail.com>
2021-10-21 21:49:18 -0700

    Add final unit tests.

--- src/utils/audio/style/test.js
+++ src/utils/audio/style/test.js
@@ -1,4 +1,4 @@
-import { getAnimatedStyleConfig, getAnimationName, getAttacks } from '.'
+import { getAnimatedStyleConfig, getAnimationName, getAttacks, getKeyframesSequence } from '.'
 
 const MOCK_STYLE_CONFIG = {
     className: 'mockClassName',
@@ -61,6 +61,42 @@ describe('getAnimationName', () => {
     })
 })
 
+describe('getKeyframesSequence', () => {
+    it('returns keyframes for single attack', () => {
+        expect(getKeyframesSequence({
+            attacks: [MOCK_ATTACKS[0]],
+            styleConfig: MOCK_STYLE_CONFIG,
+            pathClassName: 'face',
+        })).toStrictEqual([
+            { fillStyle: 'pink', percentage: 9.804 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 9.805 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 21.568 },
+            { fillStyle: 'pink', percentage: 21.569 },
+        ])
+    })
+
+    it('returns keyframes for multiple attacks', () => {
+        expect(getKeyframesSequence({
+            attacks: MOCK_ATTACKS,
+            styleConfig: MOCK_STYLE_CONFIG,
+            pathClassName: 'face',
+        })).toStrictEqual([
+            { fillStyle: 'pink', percentage: 9.804 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 9.805 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 21.568 },
+            { fillStyle: 'pink', percentage: 21.569 },
+            { fillStyle: 'pink', percentage: 34.314 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 34.315 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 46.077 },
+            { fillStyle: 'pink', percentage: 46.078 },
+            { fillStyle: 'pink', percentage: 58.824 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 58.825 },
+            { fillStyle: 'hsl(30,73%,82%)', percentage: 70.587 },
+            { fillStyle: 'pink', percentage: 70.588 },
+        ])
+    })
+})
+
 describe('getAnimatedStyleConfig', () => {
     it('returns style config for no played config', () => {
         expect(getAnimatedStyleConfig(
@@ -68,49 +104,91 @@ describe('getAnimatedStyleConfig', () => {
         )).toStrictEqual(MOCK_STYLE_CONFIG)
     })
 
-    it.skip('returns style for played pitch index config', () => {
+    it('returns style for played pitch index config', () => {
         expect(getAnimatedStyleConfig(
             MOCK_STYLE_CONFIG,
             MOCK_PITCH_INDEX_CONFIG,
         )).toStrictEqual({
-            className: 'mockClassName_0_1',
+            className: 'mockClassName_01_1',
             keyframes: [
                 {
-                    animationName: 'mockClassName_edge_0_1',
+                    animationName: 'mockClassName_edge_01_1',
                     sequence: [
-                        { percentage: 0, fillStyle: 'red' },
+                        { fillStyle: 'red', percentage: 9.804 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 9.805 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 21.568 },
+                        { fillStyle: 'red', percentage: 21.569 },
                     ],
                 },
                 {
-                    animationName: 'mockClassName_face_0_1',
+                    animationName: 'mockClassName_face_01_1',
                     sequence: [
-                        { percentage: 0, fillStyle: 'red' },
+                        { fillStyle: 'pink', percentage: 9.804 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 9.805 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 21.568 },
+                        { fillStyle: 'pink', percentage: 21.569 },
                     ],
                 },
             ],
+            styles: {
+                animation: {
+                    edge: 'mockClassName_edge_01_1 1.02s',
+                    face: 'mockClassName_face_01_1 1.02s',
+                },
+                fill: { edge: 'red', face: 'pink' },
+            },
         })
     })
 
-    it.skip('returns style for played pitch config', () => {
+    it('returns style for played pitch config', () => {
         expect(getAnimatedStyleConfig(
             MOCK_STYLE_CONFIG,
             MOCK_PITCH_CONFIG,
         )).toStrictEqual({
-            className: 'mockClassName_0_1',
+            className: 'mockClassName_01_3',
             keyframes: [
                 {
-                    animationName: 'mockClassName_edge_0_1',
+                    animationName: 'mockClassName_edge_01_3',
                     sequence: [
-                        { percentage: 0, fillStyle: 'red' },
+                        { fillStyle: 'red', percentage: 9.804 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 9.805 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 21.568 },
+                        { fillStyle: 'red', percentage: 21.569 },
+                        { fillStyle: 'red', percentage: 34.314 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 34.315 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 46.077 },
+                        { fillStyle: 'red', percentage: 46.078 },
+                        { fillStyle: 'red', percentage: 58.824 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 58.825 },
+                        { fillStyle: 'hsl(30,73%,72%)', percentage: 70.587 },
+                        { fillStyle: 'red', percentage: 70.588 },
                     ],
                 },
                 {
-                    animationName: 'mockClassName_face_0_1',
+                    animationName: 'mockClassName_face_01_3',
                     sequence: [
-                        { percentage: 0, fillStyle: 'red' },
+                        { fillStyle: 'pink', percentage: 9.804 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 9.805 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 21.568 },
+                        { fillStyle: 'pink', percentage: 21.569 },
+                        { fillStyle: 'pink', percentage: 34.314 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 34.315 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 46.077 },
+                        { fillStyle: 'pink', percentage: 46.078 },
+                        { fillStyle: 'pink', percentage: 58.824 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 58.825 },
+                        { fillStyle: 'hsl(30,73%,82%)', percentage: 70.587 },
+                        { fillStyle: 'pink', percentage: 70.588 },
                     ],
                 },
             ],
+            styles: {
+                animation: {
+                    edge: 'mockClassName_edge_01_3 1.02s',
+                    face: 'mockClassName_face_01_3 1.02s',
+                },
+                fill: { edge: 'red', face: 'pink' },
+            },
         })
     })
 })
