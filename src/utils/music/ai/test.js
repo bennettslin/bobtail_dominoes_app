6e8f2251eb3bd789a6ad12468f10963ad8f16fd3
Bennett Lin <bennettslin@gmail.com>
2021-12-12 16:40:17 -0800

    Add unit test.

--- src/utils/music/ai/test.js
+++ src/utils/music/ai/test.js
@@ -31,7 +31,24 @@ describe('getValidPointedMoves', () => {
         expect(getValidPointedMoves({
             dominoIndex: 20, board: [MOCK_BOARD[0]], limit: 3,
         })).toStrictEqual([
-            { dominoIndex: 20, pitchSets: [new Set([4, 1, 10])], placement: [[0, 1], [-1, 2]], points: 3 }, { dominoIndex: 20, pitchSets: [new Set([1, 10, 4])], placement: [[1, -2], [1, -1]], points: 3 }, { dominoIndex: 20, pitchSets: [new Set([4, 1, 10])], placement: [[1, 1], [1, 2]], points: 3 },
+            {
+                dominoIndex: 20,
+                pitchSets: [new Set([4, 1, 10])],
+                placement: [[0, 1], [-1, 2]],
+                points: 3,
+            },
+            {
+                dominoIndex: 20,
+                pitchSets: [new Set([1, 10, 4])],
+                placement: [[1, -2], [1, -1]],
+                points: 3,
+            },
+            {
+                dominoIndex: 20,
+                pitchSets: [new Set([4, 1, 10])],
+                placement: [[1, 1], [1, 2]],
+                points: 3,
+            },
         ])
     })
 
@@ -39,7 +56,18 @@ describe('getValidPointedMoves', () => {
         expect(getValidPointedMoves({
             dominoIndex: 20, board: [MOCK_BOARD[0]], limit: 2,
         })).toStrictEqual([
-            { dominoIndex: 20, pitchSets: [new Set([4, 1, 10])], placement: [[0, 1], [-1, 2]], points: 3 }, { dominoIndex: 20, pitchSets: [new Set([1, 10, 4])], placement: [[1, -2], [1, -1]], points: 3 },
+            {
+                dominoIndex: 20,
+                pitchSets: [new Set([4, 1, 10])],
+                placement: [[0, 1], [-1, 2]],
+                points: 3,
+            },
+            {
+                dominoIndex: 20,
+                pitchSets: [new Set([1, 10, 4])],
+                placement: [[1, -2], [1, -1]],
+                points: 3,
+            },
         ])
     })
 
@@ -47,7 +75,26 @@ describe('getValidPointedMoves', () => {
         expect(getValidPointedMoves({
             dominoIndex: 20, board: MOCK_BOARD, limit: 2,
         })).toStrictEqual([
-            { dominoIndex: 20, pitchSets: [new Set([9, 1, 6]), new Set([7, 5, 1, 10]), new Set([4, 9, 1, 5]), new Set([11, 2, 6, 10])], placement: [[-1, 2], [-1, 3]], points: 15 }, { dominoIndex: 20, pitchSets: [new Set([0, 5, 9, 1]), new Set([3, 7, 0, 10])], placement: [[-3, 3], [-3, 2]], points: 8 },
+            {
+                dominoIndex: 20,
+                pitchSets: [
+                    new Set([9, 1, 6]),
+                    new Set([7, 5, 1, 10]),
+                    new Set([4, 9, 1, 5]),
+                    new Set([11, 2, 6, 10]),
+                ],
+                placement: [[-1, 2], [-1, 3]],
+                points: 15,
+            },
+            {
+                dominoIndex: 20,
+                pitchSets: [
+                    new Set([0, 5, 9, 1]),
+                    new Set([3, 7, 0, 10]),
+                ],
+                placement: [[-3, 3], [-3, 2]],
+                points: 8,
+            },
         ])
     })
 })
@@ -121,6 +168,42 @@ describe('getBestPointedMovesForTurn', () => {
                 pitchSets: [new Set([11, 3, 7, 0])],
                 placement: [[1, -3], [1, -2]],
                 points: 4,
-            }])
+            },
+        ])
+    })
+
+    it('returns worse moves when specified', () => {
+        expect(getBestPointedMovesForTurn({
+            hand: new Set([18, 20, 38]),
+            board: MOCK_BOARD,
+            moveRank: 10,
+        })).toStrictEqual([
+            {
+                dominoIndex: 20,
+                pitchSets: [
+                    new Set([9, 1, 6]),
+                    new Set([7, 5, 1, 10]),
+                    new Set([4, 9, 1, 5]),
+                    new Set([11, 2, 6, 10]),
+                ],
+                placement: [[-1, 2], [-1, 3]],
+                points: 15,
+            },
+            {
+                dominoIndex: 18,
+                pitchSets: [
+                    new Set([8, 1, 5, 10]),
+                    new Set([0, 5, 9, 1]),
+                ],
+                placement: [[-3, 3], [-4, 3]],
+                points: 8,
+            },
+            {
+                dominoIndex: 38,
+                pitchSets: [new Set([3, 11, 8])],
+                placement: [[-4, 1], [-4, 2]],
+                points: 3,
+            },
+        ])
     })
 })
