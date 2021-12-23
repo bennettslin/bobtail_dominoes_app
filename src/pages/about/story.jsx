d01bc66614e2136638849b2fbce8b2d219f43419
Bennett Lin <bennettslin@gmail.com>
2021-12-22 21:56:02 -0800

    Revise about content.

--- src/pages/about/story.jsx
+++ src/pages/about/story.jsx
@@ -13,15 +13,15 @@ const Component = () => (
             titleHeading: 'Our story',
             body: [
                 `
-Hi, I'm Bennett from the Bobtail Yearlings! Some time ago, I decided that visualizing chordal space was the next phase in my growth as a songwriter. After struggling to get started, I realized that I needed to gamify the process. That's when the idea for Bobtail Dominoes hit me, and I made this prototype from mosaic bathroom tiles.
+Hi, I'm Bennett from the Bobtail Yearlings! Some time ago, I realized that in order to grow as a songwriter, I needed to visualize chordal space. After struggling to get started, I decided to gamify the process. That's when the idea for Bobtail Dominoes came to me, and I made this prototype from mosaic bathroom tiles.
                 `,
                 (
                     <Image {...{ src: prototype_reduced }} />
                 ),
                 `
-Each day, I would challenge myself to arrange the dominoes into chords. I would then strum the chords on my guitar, hoping to come up with a nice progression for my next song. As time went on, I began to dream in chordal space, and I could feel myself developing a keen harmonic intuition.
+Each day, I would challenge myself to arrange the dominoes into chords. I would then strum the chords on my guitar, hoping to come up with a nice progression for my next song. As time went on, I began to dream in chordal space, and I could feel myself developing a better harmonic intuition.
 
-Of course, it was obvious all along that Bobtail Dominoes could be a tabletop game. After play testing with some musician friends, I came up with a set of rules that we all thought worked well. My hope is that a few of you will invent your own variants and share them, just like with classic dominoes!
+Of course, it was obvious all along that Bobtail Dominoes could be a tabletop game for multiple players. So I play tested with some musician friends, and we came up with a set of rules that worked well. My hope is that a few of you will invent your own variants and share them, just like with classic dominoes!
                 `,
             ],
         }}
