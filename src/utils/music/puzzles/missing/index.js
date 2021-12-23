85fcc70c1aeb1f2fb7e01d9ada49e7c6e60f32fb
Bennett Lin <bennettslin@gmail.com>
2021-12-23 13:27:47 -0800

    Begin other puzzle types.

--- src/utils/music/puzzles/missing/index.js
+++ src/utils/music/puzzles/missing/index.js
@@ -1,11 +1,53 @@
 import { getRandomInteger } from '../../../general/random'
-import { getInitialBoardForPuzzle } from '../general'
+import { getBestMoveForBoard, getInitialBoardForPuzzle } from '../general'
+import { logTrial } from '../log'
 import { MISSING_PUZZLE_TYPE } from '../../../../constants/music/puzzle'
 
-export const getMissingPuzzle = () => {
-    return getInitialBoardForPuzzle({
-        dominoesCount: getRandomInteger(4, 7),
-        moveRank: getRandomInteger(0, 5),
+const TRIALS_COUNT = 1000
+
+export const getMissingPuzzle = ({
+    dominoesCount = getRandomInteger(4, 7),
+    minPoints = getRandomInteger(10, 15),
+    moveRank = getRandomInteger(0, 5),
+
+} = {}) => {
+    let validBoard,
+        validMove,
+        trialIndex = 0
+
+    logServe(`Finding a unique outcome with at least ${minPoints} points…`)
+
+    while (!validBoard && !validMove && trialIndex < TRIALS_COUNT) {
+        const {
+            board,
+            pool,
+        } = getInitialBoardForPuzzle({
+            dominoesCount,
+            moveRank,
+        })
+
+        if (board) {
+            const { move, ...rest } = getBestMoveForBoard({
+                board,
+                pool,
+                minPoints,
+                needsUniqueHighest: true,
+            })
+
+            logTrial({ trialIndex, ...rest })
+
+            if (move) {
+                validBoard = board
+                validMove = move
+            }
+        }
+
+        trialIndex++
+    }
+
+    return validMove ? {
+        board: validBoard,
+        moves: [validMove],
         puzzleType: MISSING_PUZZLE_TYPE,
-    })
+    } : {}
 }
