c66a8233514edd6fc10cd7fc4c1d1f28e82bb3dc
Bennett Lin <bennettslin@gmail.com>
2022-01-04 18:07:08 -0800

    Fix unit tests.

--- src/utils/pages/puzzles/test.js
+++ src/utils/pages/puzzles/test.js
@@ -1,29 +1,52 @@
 import { addPuzzleLinksForYears, addPuzzlesForMonths } from '.'
-import { DATE_MAPPED_PAGES } from '../../../__mocks__/dateMapped'
+
+const DATE_MAPPED_PUZZLE_PAGES = {
+    1983: {
+        5: {
+            4: {
+                date: { year: 1983, month: 5, day: 4 },
+                board: [],
+                topLevelPage: 'puzzles',
+            },
+        },
+    },
+    1988: {
+        8: {
+            22: {
+                date: { year: 1988, month: 8, day: 22 },
+                board: [],
+                topLevelPage: 'puzzles',
+            },
+        },
+    },
+}
 
 describe('addPuzzlesForMonths', () => {
     it('adds puzzles for months', () => {
         expect(addPuzzlesForMonths({
             configs: [
-                {
-                    date: { year: 1983, month: 5 },
-                    board: [],
-                },
-                {
-                    date: { year: 2021, month: 11 },
-                    board: [],
-                },
-                {
-                    date: { year: 2021, month: 12 },
-                    board: [],
-                },
-                {
-                    date: { year: 2022, month: 1 },
-                    board: [],
-                },
+                { date: { year: 1983, month: 5 } },
+                { date: { year: 1988, month: 8 } },
             ],
-            dateMappedPages: DATE_MAPPED_PAGES,
-        })).toStrictEqual()
+            dateMappedPages: DATE_MAPPED_PUZZLE_PAGES,
+        })).toStrictEqual([
+            {
+                date: { month: 5, year: 1983 },
+                puzzlesList: [{
+                    board: [],
+                    date: { day: 4, month: 5, year: 1983 },
+                    topLevelPage: 'puzzles',
+                }],
+            },
+            {
+                date: { month: 8, year: 1988 },
+                puzzlesList: [{
+                    board: [],
+                    date: { day: 22, month: 8, year: 1988 },
+                    topLevelPage: 'puzzles',
+                }],
+            },
+        ])
     })
 })
 
@@ -31,9 +54,13 @@ describe('addPuzzleLinksForYears', () => {
     it('adds puzzle links for years', () => {
         expect(addPuzzleLinksForYears({
             configs: [
-
+                { date: { year: 1983 } },
+                { date: { year: 1988 } },
             ],
-            dateMappedPages: DATE_MAPPED_PAGES,
-        })).toStrictEqual([])
+            dateMappedPages: DATE_MAPPED_PUZZLE_PAGES,
+        })).toStrictEqual([
+            { date: { year: 1983 }, monthsList: [5] },
+            { date: { year: 1988 }, monthsList: [8] },
+        ])
     })
 })
