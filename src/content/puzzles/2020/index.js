f50d9b9fde3080196ba693494eeabf1d556e9270
Bennett Lin <bennettslin@gmail.com>
2022-01-06 18:27:41 -0800

    Begin generating puzzles.

--- src/content/puzzles/2020/index.js
+++
@@ -1,7 +0,0 @@
-// import puzzles09 from './9'
-import puzzles10 from './10'
-// import puzzles12 from './12'
-
-export default {
-    10: puzzles10,
-}
--- src/content/puzzles/index.js
+++ src/content/puzzles/index.js
@@ -1,12 +1,10 @@
 import { populateDateMappedPages } from '../../utils/pages/dateMapped/populate'
 import { PUZZLES_PAGE } from '../../constants/pages'
-import puzzles2020 from './2020'
 import puzzles2021 from './2021'
 import puzzles2022 from './2022'
 
 const puzzles = populateDateMappedPages({
     dateMappedPages: {
-        2020: puzzles2020,
         2021: puzzles2021,
         2022: puzzles2022,
     },
