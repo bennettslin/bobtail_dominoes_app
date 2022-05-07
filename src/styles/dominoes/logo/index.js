bb4a17a07225637693aa5e4acffefeec22275a68
Bennett Lin <bennettslin@gmail.com>
2022-05-07 00:45:09 -0700

    Change to Chicle font in logo svg.

--- src/styles/dominoes/logo/index.js
+++ src/styles/dominoes/logo/index.js
@@ -3,8 +3,8 @@ import { hsl } from '../../../utils/svgs'
 
 export default {
     fill: {
-        ranchoShadow:   hsl(0, 3, 32, 0.7),
-        rancho:         hsl(60, 3, 92),
+        chicleShadow:   hsl(0, 3, 32, 0.7),
+        chicle:         hsl(60, 3, 92),
 
         logoShadow:     hsl(60, 18, 37),
         logo:           hsl(60, 23, 72),
