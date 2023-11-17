56b0633e72d5007d77c19532ba529a4b923888f5
Bennett Lin <bennettslin@gmail.com>
2023-11-16 23:52:48 -0800

    Update people colors.

--- src/styles/people/allPeople/index.js
+++ src/styles/people/allPeople/index.js
@@ -5,13 +5,13 @@ export default {
     fill: {
         sclera: hsl(216, 16, 88),
         pupil: hsl(0, 0, 0, 0.5),
-        irisBrown: hsl(42, 21, 47),
-        irisDarkBrown: hsl(36, 18, 42),
+        irisBrown: hsl(42, 18, 43),
+        irisDarkBrown: hsl(36, 17, 40),
         gum: hsl(6, 37, 63),
-        lipFemaleMedium: hsl(1, 27, 51, 0.5),
-        lipFemaleMedium__noStroke: hsl(1, 27, 51, 0.5),
-        lipFemaleBrown: hsl(2, 24, 44, 0.52),
-        lipFemaleBrown__noStroke: hsl(2, 24, 44, 0.52),
+        lipFemaleMedium: hsl(1, 27, 50, 0.5),
+        lipFemaleMedium__noStroke: hsl(1, 27, 50, 0.5),
+        lipFemaleBrown: hsl(2, 24, 43, 0.52),
+        lipFemaleBrown__noStroke: hsl(2, 24, 43, 0.52),
         lipMaleMedium__noStroke: hsl(1, 17, 54, 0.4),
         lipMaleDark__noStroke: hsl(2, 15, 41, 0.45),
         teeth: hsl(40, 17, 82),
@@ -19,7 +19,7 @@ export default {
         mouthInner: hsl(4, 21, 39),
 
         skinEastAsian: hsl(29, 45, 68),
-        skinLatino: hsl(24, 42, 62),
+        skinLatino: hsl(24, 40, 59),
         skinIndian: hsl(21, 33, 52),
         skinBlack: hsl(29, 31, 48),
 
